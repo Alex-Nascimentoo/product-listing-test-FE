814eaf69b978c124f914feb3b21ee678d5bc0af2
Alex <alexdavi150@gmail.com>
2022-01-11 17:58:48 -0300

    adding some style to the error page

--- src/pages/productAddFail.jsx
+++ src/pages/productAddFail.jsx
@@ -1,12 +1,22 @@
 import { useLocation } from 'react-router-dom'
+import '../css/failPage.css'
 
 function ProductFail() {
   const search = useLocation().search
   const message = new URLSearchParams(search).get('message')
 
-  alert(message)
+  return (
+    <main className="container row">
+      <h1>An error ocurred</h1>
+      <h3>{ message }</h3>
 
-  document.location = '/add-product'
+      <div className="buttons">
+        <a href="/add-product" className="route-button">Try again</a>
+        
+        <a href="/" className="route-button">Home page</a>
+      </div>
+    </main>
+  )
 }
 
 export default ProductFail
