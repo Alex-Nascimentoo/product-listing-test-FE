bd11dff1bc5510f0215a53ef4cb74db58dbb9fde
Alex <alexdavi150@gmail.com>
2022-01-10 23:27:55 -0300

    changing variables to work in local machine

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,4 @@
 // storing url variable
 
-export const BE_URL = 'http://pimentatestando.com'
+// export const BE_URL = 'http://pimentatestando.com'
+export const BE_URL = 'http://localhost/PHP/scandiweb-test'
